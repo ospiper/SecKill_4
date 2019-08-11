f3f3e249bca32a3091088fa15f8b4fae6a7fd285
LLAP <ospiper@foxmail.com>
2019-08-11 20:14:18 +0800

    implement /product

--- src/main/java/org/bytecamp19/seckill4/controller/MainController.java
+++ src/main/java/org/bytecamp19/seckill4/controller/MainController.java
@@ -1,6 +1,8 @@
 package org.bytecamp19.seckill4.controller;
 
+import org.apache.ibatis.annotations.Param;
 import org.bytecamp19.seckill4.entity.Product;
+import org.bytecamp19.seckill4.error.ForbiddenException;
 import org.bytecamp19.seckill4.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,27 +25,35 @@ public class MainController {
     private ProductService productService;
 
     @GetMapping("product")
-    public List<Product> getProduct() {
-        return null;
+    public Product getProduct(@Param("pid") Integer pid) throws ForbiddenException {
+        if (pid == null) {
+            throw new ForbiddenException("pid not given");
+        }
+        Product ret = productService.getProduct(pid);
+        System.out.println(ret);
+        if (ret == null) {
+            throw new ForbiddenException("Product not found");
+        }
+        return ret;
     }
 
     @PostMapping("order")
-    public HashMap<String, Object> placeOrder() {
+    public HashMap<String, Object> placeOrder() throws ForbiddenException {
         return null;
     }
 
     @PostMapping("pay")
-    public HashMap<String, Object> payOrder() {
+    public HashMap<String, Object> payOrder() throws ForbiddenException {
         return null;
     }
 
     @GetMapping("result")
-    public HashMap<String, Object> getResult() {
+    public HashMap<String, Object> getResult() throws ForbiddenException {
         return null;
     }
 
     @PostMapping("reset")
-    public HashMap<String, Object> reset() {
+    public HashMap<String, Object> reset() throws ForbiddenException {
         return null;
     }
 }
