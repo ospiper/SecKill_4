b74add5f55af775d63c7d74b1527318206828941
LLAP <ospiper@foxmail.com>
2019-08-11 23:56:43 +0800

    check session and product existence before placing orders

--- src/main/java/org/bytecamp19/seckill4/controller/MainController.java
+++ src/main/java/org/bytecamp19/seckill4/controller/MainController.java
@@ -5,12 +5,16 @@ import com.alibaba.fastjson.JSONObject;
 import org.apache.ibatis.annotations.Param;
 import org.bytecamp19.seckill4.entity.Order;
 import org.bytecamp19.seckill4.entity.Product;
+import org.bytecamp19.seckill4.entity.Session;
 import org.bytecamp19.seckill4.error.ForbiddenException;
 import org.bytecamp19.seckill4.service.OrderService;
 import org.bytecamp19.seckill4.service.ProductService;
+import org.bytecamp19.seckill4.service.SessionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.*;
 
 /**
@@ -44,15 +48,37 @@ public class MainController {
     }
 
     @PostMapping("order")
-    public JSONObject placeOrder(@RequestBody JSONObject json) throws ForbiddenException {
+    public JSONObject placeOrder(@RequestBody JSONObject json, HttpServletRequest request) throws ForbiddenException {
         Integer pid = json.getInteger("pid");
         Integer uid = json.getInteger("uid");
         if (uid == null || pid == null) {
             throw new ForbiddenException("pid or uid not given");
         }
+
+        // Check session
+        Session session = sessionService.getSession(request.getHeader("sessionid"), uid);
+        if (debug) {
+            System.out.println("pid = " + pid);
+            System.out.println("uid = " + uid);
+            System.out.println("session = " + session);
+        }
+        if (session == null) {
+            throw new ForbiddenException("Session not found");
+        }
+
+        // Check product
+        Product product = productService.getProduct(pid);
+        if (debug) {
+            System.out.println("Product = " + product);
+        }
+        if (product == null) {
+            throw new ForbiddenException("Product not found");
+        }
+
         // Place an order
-        Order order = orderService.placeOrder(pid, uid);
+        Order order = orderService.placeOrder(uid, product);
         // Returns null if there is no remaining products
+
         JSONObject ret = new JSONObject();
         if (order == null) {
             ret.put("code", 1);
@@ -64,7 +90,18 @@ public class MainController {
     }
 
     @PostMapping("pay")
-    public HashMap<String, Object> payOrder() throws ForbiddenException {
+    public JSONObject payOrder(@RequestBody JSONObject json) throws ForbiddenException {
+        Integer uid = json.getInteger("uid");
+        Integer price = json.getInteger("price");
+        String orderId = json.getString("order_id");
+        if (uid == null || price == null || orderId == null) {
+            throw new ForbiddenException("pid / price / order_id not given");
+        }
+        int pid = orderService.validateOrderId(orderId, uid, price);
+        if (pid < 0) {
+            throw new ForbiddenException("Invalid order_id");
+        }
+
         return null;
     }
 
