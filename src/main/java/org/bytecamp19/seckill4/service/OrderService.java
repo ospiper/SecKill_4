87b40c48ae626b6bee9d7fd3d4c071b87d26d61c
LLAP <ospiper@foxmail.com>
2019-08-20 00:16:38 +0800

    wrap OrderId object for better coding in validating order ids

--- src/main/java/org/bytecamp19/seckill4/service/OrderService.java
+++ src/main/java/org/bytecamp19/seckill4/service/OrderService.java
@@ -1,8 +1,10 @@
 package org.bytecamp19.seckill4.service;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import org.bytecamp19.seckill4.cache.InventoryManager;
-import org.bytecamp19.seckill4.cache.OrderLimitManager;
+import lombok.Data;
+import lombok.Getter;
+import org.bytecamp19.seckill4.cache.*;
+import org.bytecamp19.seckill4.entity.OrderIdWrapper;
 import org.bytecamp19.seckill4.entity.OrderResult;
 import org.bytecamp19.seckill4.entity.Order;
 import org.bytecamp19.seckill4.entity.Product;
@@ -10,8 +12,7 @@ import org.bytecamp19.seckill4.error.ForbiddenException;
 import org.bytecamp19.seckill4.mapper.OrderMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -28,16 +29,21 @@ public class OrderService {
     private OrderMapper orderMapper;
     private OrderLimitManager limitManager;
     private InventoryManager inventoryManager;
+    private RedisMessageQueue mq;
 
     private static Random random = new Random();
 
     public OrderService(ProductService productService, OrderMapper orderMapper,
-                        OrderLimitManager limitManager, InventoryManager inventoryManager) {
+                        OrderLimitManager limitManager, InventoryManager inventoryManager,
+                        RedisMessageQueue mq) {
         this.productService = productService;
         this.orderMapper = orderMapper;
         this.limitManager = limitManager;
         this.inventoryManager = inventoryManager;
+        this.mq = mq;
     }
+
+
 
     /**
      * Generate an order_id with given arguments
@@ -71,35 +77,16 @@ public class OrderService {
      * @param price price of the product
      * @return pid of the product, &lt; 0 if not valid; -1 (length), -2(uid), -3(check / price)
      */
-    public int validateOrderId(String orderId, int uid, int price) {
-        String[] segments = orderId.split("\\.");
-        logger.debug("Arg count = " + segments.length);
-        logger.debug("Validating " + orderId);
-        if (segments.length != 5) return -1;
-
-        long r_timestamp = Long.parseLong(segments[0]);
-        int r_uid = Integer.parseInt(segments[1]);
-        logger.debug("Timestamp = " + r_timestamp);
-        logger.debug("UID = " + r_uid);
-        if (uid != r_uid) return -2;
-        int r_pid = Integer.parseInt(segments[2]);
-        int r_rand = Integer.parseInt(segments[3]);
-        int r_check = Integer.parseInt(segments[4]);
-        logger.debug("PID = " + r_pid);
-        logger.debug("Rand = " + r_rand);
-        logger.debug("Check = " + r_check);
-        // validate check pattern
-        int check = 0;
-        for (int index = 0; index < segments.length - 1; ++index) {
-            String s = segments[index];
-            for (int i = 0; i < s.length(); ++i) {
-                check ^= s.charAt(i) - '0';
-            }
+    public OrderIdWrapper validateOrderId(String orderId, int uid, int price) {
+        OrderIdWrapper id = null;
+        try {
+            id = new OrderIdWrapper(orderId);
         }
-        check ^= price;
-        logger.debug("Expected check = " + check);
-        if (check != r_check) return -3;
-        return r_pid;
+        catch (IllegalArgumentException ex) {
+            return null;
+        }
+        if (id.getUid() != uid || id.getPrice() != price) return null;
+        return id;
     }
 
     public Order placeOrder(int uid, Product p) throws ForbiddenException {
@@ -131,7 +118,6 @@ public class OrderService {
         // TODO: 1. 下订单时扣减完库存直接返回订单id，把订单加入到队列里慢慢写入，写入数据库的时候直接获取到支付id
         // TODO: 2. 支付时如果order_id合法，先查询有没有写数据，如果查询到就直接返回，如果没查询到就按照用户提交的数据发送http请求（Spring WebFlux WebClient）
         // TODO: 3. 支付时如果没查到order，需要向队列推送一条标记，该订单已经支付过（并记录支付token），在worker写数据时查询该标记并更新数据。
-        return null;
     }
 
     public List<OrderResult> getOrdersByUid(int uid){
