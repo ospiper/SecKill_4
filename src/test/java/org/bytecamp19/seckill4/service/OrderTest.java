3e8e9e112f09bd3b9f239379a05520d6f7f9876e
LLAP <ospiper@foxmail.com>
2019-08-22 02:11:36 +0800

    add tests

--- src/test/java/org/bytecamp19/seckill4/service/OrderTest.java
+++ src/test/java/org/bytecamp19/seckill4/service/OrderTest.java
@@ -3,7 +3,9 @@ package org.bytecamp19.seckill4.service;
 import org.bytecamp19.seckill4.cache.InventoryManager;
 import org.bytecamp19.seckill4.cache.OrderLimitManager;
 import org.bytecamp19.seckill4.cache.OrderMessage;
+import org.bytecamp19.seckill4.cache.RedisMessageQueue;
 import org.bytecamp19.seckill4.controller.MainController;
+import org.bytecamp19.seckill4.entity.Order;
 import org.bytecamp19.seckill4.entity.OrderIdWrapper;
 import org.bytecamp19.seckill4.entity.Product;
 import org.bytecamp19.seckill4.error.ForbiddenException;
@@ -36,6 +38,8 @@ public class OrderTest {
     private InventoryManager inventoryManager;
     @Autowired
     private OrderLimitManager orderLimitManager;
+    @Autowired
+    private RedisMessageQueue mq;
 
     private final int pid = 177620431;
     private final int uid = 175230;
@@ -44,6 +48,7 @@ public class OrderTest {
     public void before() {
         orderLimitManager.clearLimits();
         inventoryManager.clearInventory();
+        mq.clear();
     }
 
     @Test
@@ -76,9 +81,36 @@ public class OrderTest {
         }
     }
 
-    @After
-    public void after() {
-        orderLimitManager.clearLimits();
-        inventoryManager.clearInventory();
+    @Test
+    public void requestTokenTest() {
+        final String orderId = "1566405592619.175230.177620431.173.101";
+        OrderIdWrapper wrapper = new OrderIdWrapper(orderId);
+        orderService.getToken(wrapper);
+    }
+
+    @Test
+    public void unWrittenPayTest() {
+        Product product = productService.getProduct(pid);
+        try {
+            OrderMessage message = orderService.placeOrder(uid, product);
+            System.out.println(message);
+            OrderIdWrapper wrapper = new OrderIdWrapper(message.getOrder_id());
+            Order o = orderService.payOrder(wrapper);
+            System.out.println(o);
+        }
+        catch (ForbiddenException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @Test
+    public void emptyTokenTest() {
+        try {
+           OrderIdWrapper id = new OrderIdWrapper("1566408601137.175230.177620431.625.110");
+           orderService.payOrder(id);
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
